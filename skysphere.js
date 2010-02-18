23fa725f523da25091ac004c1a3ae915ae007227
Giles Thomas <giles.thomas@resolversystems.com>
2010-02-18 14:49:48 +0000

    Added skysphere Moved texture code to a separate file.

--- skysphere.js
+++ skysphere.js
@@ -1,39 +1,23 @@
-function Earth(gl, location) {
+function SkySphere(gl) {
   this.gl = gl;
-  this.shininess = 32;
-  this.location = location;
-
-  this.colorMap = loadTexture("earth.jpg");  
-  this.specularMap = loadTexture("earth-specular.gif");
-  
-  this.mesh = createSphereMesh(gl, 6371);
-  
-  this.rotateAngle = 0;
+  this.colorMap = loadTexture("gigapixel-milky-way.jpg");  
+  this.mesh = createSphereMesh(gl, 1);
 }
 
 
-Earth.prototype.draw = function(shaderProgram, offset) {
+SkySphere.prototype.draw = function(shaderProgram, size) {
   if (this.mesh == null) {
     return;
   }
   
-  this.gl.mvPushMatrix();
-  
-  this.gl.mvTranslate(V3.sub(this.location, offset));
-  
-  this.gl.mvRotate(this.rotateAngle, [0, 1, 0]);
-  
-  this.gl.uniform1i(shaderProgram.useLightingUniform, true);
+  this.gl.uniform1i(shaderProgram.useLightingUniform, false);
 
   this.gl.uniform1i(shaderProgram.useColorMapUniform, true);
   this.gl.activeTexture(gl.TEXTURE0);
   this.gl.bindTexture(gl.TEXTURE_2D, this.colorMap);
   this.gl.uniform1i(shaderProgram.colorMapSamplerUniform, 0);
   
-  this.gl.uniform1i(shaderProgram.useSpecularMapUniform, true);
-  this.gl.activeTexture(gl.TEXTURE1);
-  this.gl.bindTexture(gl.TEXTURE_2D, this.specularMap);
-  this.gl.uniform1i(shaderProgram.specularMapSamplerUniform, 1);
+  this.gl.uniform1i(shaderProgram.useSpecularMapUniform, false);
 
   this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.mesh.vertexPositionBuffer);
   this.gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, this.mesh.vertexPositionBuffer.itemSize, this.gl.FLOAT, false, 0, 0);
@@ -48,10 +32,5 @@ Earth.prototype.draw = function(shaderProgram, offset) {
   setMatrixUniforms();
   this.gl.drawElements(this.gl.TRIANGLES, this.mesh.vertexIndexBuffer.numItems, this.gl.UNSIGNED_SHORT, 0);
   
-  this.gl.mvPopMatrix();
-}
-
-
-Earth.prototype.animate = function(elapsed) {
-  this.rotateAngle += (10 * elapsed) / 1000;
+  gl.clear(gl.DEPTH_BUFFER_BIT);
 }
---
+++ texture.js
@@ -0,0 +1,20 @@
+function handleLoadedTexture(texture) {
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texImage2D(gl.TEXTURE_2D, 0, texture.image);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+  gl.generateMipmap(gl.TEXTURE_2D);
+
+  gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
+
+function loadTexture(url) {
+  var result = gl.createTexture();
+  result.image = new Image();
+  result.image.onload = function() {
+    handleLoadedTexture(result)
+  };
+  result.image.src = url;
+  return result;
+}
